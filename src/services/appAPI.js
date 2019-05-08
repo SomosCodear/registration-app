3dc6ea616a5768c526ca1a31701d236af632ee6c
homer0 <leo.apiwan@olapic.com>
2019-05-08 04:54:08 -0300

    refactor(services/appAPI): change the way the endpoints for entities filter work

--- src/services/appAPI.js
+++ src/services/appAPI.js
@@ -1,6 +1,6 @@
 import { inject } from 'react-injext';
 import APIClient from 'wootils/shared/apiClient';
-import AppConfiguration from './appConfiguration';
+import { AppConfiguration } from './appConfiguration';
 
 @inject(AppConfiguration)
 class AppAPI extends APIClient {
@@ -13,6 +13,49 @@ class AppAPI extends APIClient {
     );
 
     this._fetchClient = global.fetch.bind(global);
+  }
+
+  filterCustomers(filters, takeOne = false) {
+    const query = this._createFilters(filters);
+    let result = this.get(this.endpoint('customers.filter', query));
+    if (takeOne) {
+      result = result.then((response) => this._takeOneItem(response));
+    }
+
+    return result;
+  }
+
+  filterTickets(filters, takeOne = false) {
+    const query = this._createFilters(filters);
+    let result = this.get(this.endpoint('tickets.filter', query));
+    if (takeOne) {
+      result = result.then((response) => this._takeOneItem(response));
+    }
+
+    return result;
+  }
+
+  fetch(options) {
+    return super.fetch(options)
+    .then((response) => response.data || response);
+  }
+
+  _createFilters(properties) {
+    return Object.keys(properties).reduce(
+      (acc, key) => Object.assign(acc, {
+        [`filter[${key}]`]: properties[key],
+      }),
+      {},
+    );
+  }
+
+  _takeOneItem(response = []) {
+    let result;
+    if (response.length) {
+      [result] = response;
+    }
+
+    return result;
   }
 
   _fetch(url, options = {}) {
