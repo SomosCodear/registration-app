ac2ac054561ad621d1f726929ff62b05194c9857
homer0 <leo.apiwan@olapic.com>
2019-05-08 02:26:40 -0300

    feat(config): add endpoint for searching customers

--- config/app.config.js
+++ config/app.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   api: {
     url: process.env.API_URL || 'https://checkout.webconf.tech/api',
-    endpoints: {},
+    endpoints: {
+      customers: {
+        search: 'customers',
+      },
+    },
   },
 };
