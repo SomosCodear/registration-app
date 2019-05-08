53fceef3290a01e74c41bde033f2a978051a4799
homer0 <leo.apiwan@olapic.com>
2019-05-08 05:30:07 -0300

    feat(services/tickets): add dummy method to check-in a ticket

--- src/services/tickets.js
+++ src/services/tickets.js
@@ -10,6 +10,16 @@ class Tickets {
   searchTicketByCustomerId(customerId) {
     return this._appAPI.filterTickets({ customerId }, true);
   }
+
+  checkInTicketById(ticketId) {
+    return new Promise((resolve) => {
+      // eslint-disable-next-line
+      console.log('CHECK IN', ticketId);
+      setTimeout(() => {
+        resolve(ticketId);
+      }, 1000);
+    });
+  }
 }
 
 export { Tickets };
