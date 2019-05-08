4e92c4f9a5527c7a9c368224bf0938207d442d9a
homer0 <leo.apiwan@olapic.com>
2019-05-08 02:26:07 -0300

    feat(app): set the configuration on the appConfiguration service before rendering

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { container } from 'react-injext';
 import { AppContainer } from './containers';
+import { AppConfiguration } from './services';
 
-const config = process.env.CONFIG;
-
-// eslint-disable-next-line
-console.log('Config', config);
+const [appConfiguration] = container.get(AppConfiguration);
+appConfiguration.setConfig(process.env.CONFIG);
 
 ReactDOM.render(
   <AppContainer />,
