72935ac1e84a6ec702661ca35b89bd782e5fc1ee
homer0 <leo.apiwan@olapic.com>
2019-05-08 02:25:00 -0300

    feat(project/projext): allow decorators and class properties

--- config/projext.config.js
+++ config/projext.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   targets: {
     app: {
+      includeModules: ['wootils'],
       html: {
         template: 'index.tpl.html',
+      },
+      babel: {
+        features: {
+          decorators: true,
+          classProperties: true,
+        },
       },
       configuration: {
         enabled: true,
